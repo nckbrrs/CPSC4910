b1244cc887f72a7258b108e5ba8958b912ebd884
Nick Barrs <nickbarrs@1.179.21.198.tigernet.wifi.dyn.clemson.edu>
2017-11-08 23:37:49 -0500

    separated files into student and employee

---
+++ student/sandwich-builder.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Build Your Own Sandwich</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="sandwich-builder.css"/>
+  </head>
+
+  <body>
+    <h1>Build Your Own Sandwich</h1>
+    <p id="instructions">Enter your name and phone number to begin:</p>
+    <div>
+      <form id="name-and-number" method="post" action="sandwich-builder-2.php">
+        <label>Name: <input type="text" name="name" id="name"></label><br>
+        <label>Phone Number: <input type="tel" name="phone_num" id="phone_num"></label><br>
+        <input type="hidden" name="comingBack" value="1">
+        <button id="buildYourSandwich" type="submit" name="buildYourSandwich">Build Your Sandwich</button>
+      </form>
+    </div>
+    <script type="text/javascript" src="sandwich-builder.js"></script>
+  </body>
+</html>
