b30e0feed01fb790fbb4f15166d9ea6bd26a9b4c
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-10 13:08:44 -0500

    WIP

---
+++ student/buildSandwich.js
@@ -0,0 +1,15 @@
+function onLoad() {
+  var formNode = document.getElementById("buildSandwichForm");
+  formNode.onsubmit = buildSandwichOnSubmit;
+
+  var listOfBreads = document.getElementById("chooseBread").getElementsByTagName("input");
+  for (i=0; i<listOfBreads.length; i++) {
+    console.log(listOfBreads[i].id);
+  }
+
+  if (errorMessage != "false") {
+    showError(errorMessage);
+  }
+};
+
+window.addEventListener("load", onLoad, false);
