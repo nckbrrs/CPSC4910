787a6d5939244b2d76cdd1704db1c80989791536
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-10 13:30:30 -0500

    WIP

--- student/buildSandwich.js
+++ student/buildSandwich.js
@@ -25,7 +25,7 @@ function onLoad() {
   var meatInputs = document.getElementById("chooseMeats").getElementsByTagName("input");
   for (meat in meatInputs) {
     meatInputs[meat].onclick = function() {
-      if (meatInputs[meat].checked) {
+      if ((meatInputs[meat]).checked) {
         console.log(this.value, " got checked\n");
         sandwich['meats'].push(this.value);
       } else {
