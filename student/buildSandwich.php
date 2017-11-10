af47702e97cbf7dec6a7cbd8cff31ad20c36b980
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-10 12:56:06 -0500

    WIP

--- student/buildSandwich.php
+++ student/buildSandwich.php
@@ -5,21 +5,23 @@ session_start();
 
 echo "<table>";
 foreach ($_POST as $key => $value) {
-  echo "<tr>";
-  echo "<td>";
-  echo $key;
-  echo "</td>";
-  echo "<td>";
-  if (is_array($value)) {
-    foreach($_POST[$key] as $value) {
+  if ($key == "bread" || $key == "meats" || $key == "veggies" || $key == "cheeses" || $key == "sauces") {
+    echo "<tr>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "<td>";
+    if (is_array($value)) {
+      foreach($_POST[$key] as $value) {
+        echo $value;
+        echo ", ";
+      }
+    } else {
       echo $value;
-      echo ", ";
     }
-  } else {
-    echo $value;
+    echo "</td>";
+    echo "</tr>";
   }
-  echo "</td>";
-  echo "</tr>";
 }
 echo "</table";
 
@@ -69,23 +71,23 @@ echo "</table";
           <div id="chooseMeats">
             <h3>Choose your meats:</h3>
             <ul>
-              <li><input type="checkbox" name="meat[]" value="turkey" id="turkey" />
+              <li><input type="checkbox" name="meats[]" value="turkey" id="turkey" />
                 <label for="turkey"><img src="/cpsc4910/student/img/meats/turkey.png"/></label>
               </li>
-              <li><input type="checkbox" name="meat[]" value="ham" id="ham" />
+              <li><input type="checkbox" name="meats[]" value="ham" id="ham" />
                 <label for="ham"><img src="/cpsc4910/student/img/meats/ham.png"/></label>
               </li>
-              <li><input type="checkbox" name="meat[]" value="salami" id="salami" />
+              <li><input type="checkbox" name="meats[]" value="salami" id="salami" />
                 <label for="salami"><img src="/cpsc4910/student/img/meats/salami.png"/></label>
               </li>
               <br>
-              <li><input type="checkbox" name="meat[]" value="tuna salad" id="tunaSalad" />
+              <li><input type="checkbox" name="meats[]" value="tuna salad" id="tunaSalad" />
                 <label for="tunaSalad"><img src="/cpsc4910/student/img/meats/tunaSalad.png"/></label>
               </li>
-              <li><input type="checkbox" name="meat[]" value="chicken salad" id="chickenSalad" />
+              <li><input type="checkbox" name="meats[]" value="chicken salad" id="chickenSalad" />
                 <label for="chickenSalad"><img src="/cpsc4910/student/img/meats/chickenSalad.png"/></label>
               </li>
-              <li><input type="checkbox" name="meat[]" value="bacon" id="bacon" />
+              <li><input type="checkbox" name="meats[]" value="bacon" id="bacon" />
                 <label for="bacon"><img src="/cpsc4910/student/img/meats/bacon.png"/></label>
               </li>
             </ul>
@@ -93,23 +95,23 @@ echo "</table";
           <div id="chooseCheeses">
             <h3>Choose your cheeses:</h3>
             <ul>
-              <li><input type="checkbox" value="american" id="american" />
+              <li><input type="checkbox" name="cheeses[]" value="american" id="american" />
                 <label for="american"><img src="/cpsc4910/student/img/cheeses/american.png" /></label>
               </li>
-              <li><input type="checkbox" value="swiss" id="swiss" />
+              <li><input type="checkbox" name="cheeses[]" value="swiss" id="swiss" />
                 <label for="swiss"><img src="/cpsc4910/student/img/cheeses/swiss.png" /></label>
               </li>
-              <li><input type="checkbox" value="provolone" id="provolone" />
+              <li><input type="checkbox" name="cheeses[]" value="provolone" id="provolone" />
                 <label for="provolone"><img src="/cpsc4910/student/img/cheeses/provolone.png" /></label>
               </li>
               <br>
-              <li><input type="checkbox" value="cheddar" id="cheddar" />
+              <li><input type="checkbox" name="cheeses[]" value="cheddar" id="cheddar" />
                 <label for="cheddar"><img src="/cpsc4910/student/img/cheeses/cheddar.png" /></label>
               </li>
-              <li><input type="checkbox" value="pepper jack" id="pepperJack" />
+              <li><input type="checkbox" name="cheeses[]" value="pepper jack" id="pepperJack" />
                 <label for="pepperJack"><img src="/cpsc4910/student/img/cheeses/pepperJack.png" /></label>
               </li>
-              <li><input type="checkbox" value="mozzarella" id="mozzarella" />
+              <li><input type="checkbox" name="cheeses[]" value="mozzarella" id="mozzarella" />
                 <label for="mozzarella"><img src="/cpsc4910/student/img/cheeses/mozzarella.png" /></label>
               </li>
             </ul>
@@ -117,33 +119,33 @@ echo "</table";
           <div id="chooseVeggies">
             <h3>Choose your veggies:</h3>
             <ul>
-              <li><input type="checkbox" value="lettuce" id="lettuce" />
+              <li><input type="checkbox" name="veggies[]" value="lettuce" id="lettuce" />
                 <label for="lettuce"><img src="/cpsc4910/student/img/veggies/lettuce.png" /></label>
               </li>
-              <li><input type="checkbox" value="tomato" id="tomato" />
+              <li><input type="checkbox" name="veggies[]" value="tomato" id="tomato" />
                 <label for="tomato"><img src="/cpsc4910/student/img/veggies/tomato.png" /></label>
               </li>
-              <li><input type="checkbox" value="onions" id="onions" />
+              <li><input type="checkbox" name="veggies[]" value="onions" id="onions" />
                 <label for="onions"><img src="/cpsc4910/student/img/veggies/onions.png" /></label>
               </li>
               <br>
-              <li><input type="checkbox" value="spinach" id="spinach" />
+              <li><input type="checkbox" name="veggies[]" value="spinach" id="spinach" />
                 <label for="spinach"><img src="/cpsc4910/student/img/veggies/spinach.png" /></label>
               </li>
-              <li><input type="checkbox" value="cucumbers" id="cucumbers" />
+              <li><input type="checkbox" name="veggies[]" value="cucumbers" id="cucumbers" />
                 <label for="cucumbers"><img src="/cpsc4910/student/img/veggies/cucumbers.png" /></label>
               </li>
-              <li><input type="checkbox" value="black olives" id="blackOlives" />
+              <li><input type="checkbox" name="veggies[]" value="black olives" id="blackOlives" />
                 <label for="blackOlives"><img src="/cpsc4910/student/img/veggies/blackOlives.png" /></label>
               </li>
               <br>
-              <li><input type="checkbox" value="green peppers" id="greenPeppers" />
+              <li><input type="checkbox" name="veggies[]" value="green peppers" id="greenPeppers" />
                 <label for="greenPeppers"><img src="/cpsc4910/student/img/veggies/greenPeppers.png" /></label>
               </li>
-              <li><input type="checkbox" value="jalapeños" id="jalapeños" />
+              <li><input type="checkbox" name="veggies[]" value="jalapeños" id="jalapeños" />
                 <label for="jalapeños"><img src="/cpsc4910/student/img/veggies/jalapenos.png" /></label>
               </li>
-              <li><input type="checkbox" value="pickles" id="pickles" />
+              <li><input type="checkbox" name="veggies[]" value="pickles" id="pickles" />
                 <label for="pickles"><img src="/cpsc4910/student/img/veggies/pickles.png" /></label>
               </li>
             </ul>
@@ -151,33 +153,33 @@ echo "</table";
           <div id="chooseSauces">
             <h3>Choose your sauce(s):</h3>
             <ul>
-              <li><input type="checkbox" value="mayonnaise" id="mayonnaise" />
+              <li><input type="checkbox" name="sauces[]" value="mayonnaise" id="mayonnaise" />
                 <label for="mayonnaise"><img src="/cpsc4910/student/img/sauces/mayonnaise.png" /></label>
               </li>
-              <li><input type="checkbox" value="ranch" id="ranch" />
+              <li><input type="checkbox" name="sauces[]" value="ranch" id="ranch" />
                 <label for="ranch"><img src="/cpsc4910/student/img/sauces/ranch.png" /></label>
               </li>
-              <li><input type="checkbox" value="mustard" id="mustard" />
+              <li><input type="checkbox" name="sauces[]" value="mustard" id="mustard" />
                 <label for="mustard"><img src="/cpsc4910/student/img/sauces/mustard.png" /></label>
               </li>
               <br>
-              <li><input type="checkbox" value="chipotle" id="chipotle" />
+              <li><input type="checkbox" name="sauces[]" value="chipotle" id="chipotle" />
                 <label for="chipotle"><img src="/cpsc4910/student/img/sauces/chipotle.png" /></label>
               </li>
-              <li><input type="checkbox" value="oil and vinegar" id="oilAndVinegar" />
+              <li><input type="checkbox" name="sauces[]" value="oil and vinegar" id="oilAndVinegar" />
                 <label for="oilAndVinegar"><img src="/cpsc4910/student/img/sauces/oilAndVinegar.png" /></label>
               </li>
-              <li><input type="checkbox" value="caesar" id="caesar" />
+              <li><input type="checkbox" name="sauces[]" value="caesar" id="caesar" />
                 <label for="caesar"><img src="/cpsc4910/student/img/sauces/caesar.png" /></label>
               </li>
               <br>
-              <li><input type="checkbox" value="barbecue" id="barbecue" />
+              <li><input type="checkbox" name="sauces[]" value="barbecue" id="barbecue" />
                 <label for="barbecue"><img src="/cpsc4910/student/img/sauces/barbecue.png" /></label>
               </li>
-              <li><input type="checkbox" value="buffalo" id="buffalo" />
+              <li><input type="checkbox" name="sauces[]" value="buffalo" id="buffalo" />
                 <label for="buffalo"><img src="/cpsc4910/student/img/sauces/buffalo.png" /></label>
               </li>
-              <li><input type="checkbox" value="honey mustard" id="honeyMustard" />
+              <li><input type="checkbox" name="sauces[]" value="honey mustard" id="honeyMustard" />
                 <label for="honeyMustard"><img src="/cpsc4910/student/img/sauces/honeyMustard.png" /></label>
               </li>
             </ul>
