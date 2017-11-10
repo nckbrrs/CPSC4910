041877852c8304764f31e371c37e42fc87f16d33
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-10 13:19:45 -0500

    WIP

--- student/buildSandwich.js
+++ student/buildSandwich.js
@@ -12,7 +12,7 @@ function onLoad() {
     cheeses: [];
     veggies: [];
     sauces: [];
-  }
+  };
 
   var listOfBreads = document.getElementById("chooseBread").getElementsByTagName("input");
   for (bread in listOfBreads) {
