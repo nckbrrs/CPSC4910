11c155cafbc4ed34994d6c8e3e11e84cf9b30b5e
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-10 16:21:17 -0500

    WIP

--- student/nameNum.php
+++ student/nameNum.php
@@ -10,7 +10,7 @@ $_SESSION = array();
     <title>Sandwich Builder – Enter Your Name and Number</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    </style>
+    <link rel="stylesheet" type="text/css" href="/cpsc4910/student/nameNum.css" />
   </head>
 
   <body>
