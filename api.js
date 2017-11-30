662911f2bf3be4738a6d1fde7a83dc9ff942a9ac
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-30 00:45:17 -0500

    WIP

--- api.js
+++ api.js
@@ -11,6 +11,13 @@ bluebird.promisifyAll(redis.Multi.prototype);
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, DELETE, POST, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
+});
 
 /* Redis setup */
 var client = redis.createClient();
