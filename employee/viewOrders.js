8197f5f8a5b367b3e815523b2ab69a5133f6f1cf
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-30 11:43:00 -0500

    WIP

--- employee/viewOrders.js
+++ employee/viewOrders.js
@@ -3,8 +3,8 @@ function onLoad() {
 
   axios.get("http://54.200.82.249:3000/orders")
     .then(function (response) {
-      console.log(response['data']);
-      orderListNode.innerHTML = response['data'];
+      console.log(response['data'][0]);
+      orderListNode.innerHTML = response['data'][0];
       return true;
     })
     .catch(function (error) {
