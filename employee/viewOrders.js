5c97ada42ea3f4889b184f565ec592d8c6fb8401
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-30 12:36:42 -0500

    WIP

--- employee/viewOrders.js
+++ employee/viewOrders.js
@@ -7,9 +7,62 @@ function populateOrderList(orders) {
     newOrderDiv.setAttribute("class", "order");
     newOrderHTML = "";
     newOrderHTML += "<b>Name</b>: " + orders[order]['name'];
-    newOrderHTML += "<b>Phone Number</b>: " + orders[order]['phoneNum'];
+    newOrderHTML += "<br><b>Phone Number</b>: " + orders[order]['phoneNum'];
 
-    newOrderHTML += "<br>";
+    var sandwichHTML = "<br>";
+    sandwichHTML += "<b>Bread</b>: ";
+    if (orders[order]['bread'] == null) {
+      sandwichHTML += "none";
+    } else {
+      sandwichHTML += (orders[order]['bread']);
+    }
+    sandwichHTML += "<br>";
+
+    sandwichHTML += "<b>Meats</b>: ";
+    if (orders[order]['meats'] == null) {
+      sandwichHTML += "none";
+    } else {
+      for (meat in orders[order]['meats']) {
+        sandwichHTML += (orders[order]['meats'][meat] + ", ");
+      }
+    }
+    sandwichHTML = sandwichHTML.replace(/,\s*$/, "");
+    sandwichHTML += "<br>";
+
+    sandwichHTML += "<b>Cheeses</b>: ";
+    if (orders[order]['cheeses'] == null) {
+      sandwichHTML += "none"
+    } else {
+      for (cheese in orders[order]['cheeses']) {
+        sandwichHTML += (orders[order]['cheeses'][cheese] + ", ");
+      }
+    }
+    sandwichHTML = sandwichHTML.replace(/,\s*$/, "");
+    sandwichHTML += "<br>";
+
+    sandwichHTML += "<b>Veggies</b>: ";
+    if (orders[order]['veggies'] == null) {
+      sandwichHTML += "none"
+    } else {
+      for (veggie in orders[order]['veggies']) {
+        sandwichHTML += (orders[order]['veggies'][veggie] + ", ");
+      }
+    }
+    sandwichHTML = sandwichHTML.replace(/,\s*$/, "");
+    sandwichHTML += "<br>";
+
+    sandwichHTML += "<b>Sauces</b>: ";
+    if (orders[order]['sauces'] == null) {
+      sandwichHTML += "none"
+    } else {
+      for (sauce in orders[order]['sauces']) {
+        sandwichHTML += (orders[order]['sauces'][sauce] + ", ");
+      }
+    }
+    sandwichHTML = sandwichHTML.replace(/,\s*$/, "");
+    sandwichHTML += "<br><br>";
+
+    newOrderHTML += sandwichHTML;
     newOrderDiv.innerHTML = newOrderHTML;
     orderListNode.appendChild(newOrderDiv);
   }
