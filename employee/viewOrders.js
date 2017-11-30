70ef43959203405554973bde1427a855626373f2
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-30 11:43:54 -0500

    WIP

--- employee/viewOrders.js
+++ employee/viewOrders.js
@@ -3,8 +3,7 @@ function onLoad() {
 
   axios.get("http://54.200.82.249:3000/orders")
     .then(function (response) {
-      console.log(JSON.stringify(response['data']));
-      orderListNode.innerHTML = response['data'][0];
+      orderListNode.innerHTML = JSON.stringify(response['data']);
       return true;
     })
     .catch(function (error) {
